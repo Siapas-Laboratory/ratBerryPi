7597f863ad86b4f3aef47c9daa1408ea8e7ba84f
nathanielnyema <nyema@seas.upenn.edu>
2023-04-24 17:20:05 -0700

    simple client code

---
+++ client.py
@@ -0,0 +1,29 @@
+import socket
+
+#TODO: need something to check that the server is on
+# need to 
+
+def connect_client(host, port):
+    try:
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.connect(host, port)
+    except socket.error as msg:
+        print(msg)
+        client = None
+    return client
+
+def cli(client):
+    while True:
+        cmd = input("enter a command: ")
+        client.sendall(cmd)
+
+if __name__=='__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--host", default = 'localhost')
+    parser.add_argument("--port", defaule = '5560')
+    args = parser.parse_args()
+    client = connect_client(args.host, args.port)
+    if client is not None:
+        cli(client)
+
